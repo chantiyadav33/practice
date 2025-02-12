54d4ee8808f5e87593f3a0abaeec7adfa8492088
nit82 <nit82@192.168.1.28>
2025-02-12 06:17:25 +0530

    some practice

---
+++ practice/src/com/nit/hk/example.java
@@ -0,0 +1,26 @@
+package com.nit.hk;
+
+public class example {
+public static void main(String[] args) {
+	// printing 1 to 100 numbers in ascending order
+	for(int i =1 ;i<=100; i++) {
+		System.out.println(i);
+	}
+
+	// print 100 to 1 number in descending order
+for(int i=100; i>=1;i--) {
+	System.out.println(i);
+}
+ // print A to Z alphabits order
+     char c='a';
+	 while(c<'z'){
+		 System.out.println(c);
+		  c++;
+	  }
+	 char c1='z';
+	 while(c1>='a') {
+		 System.out.println(c1);
+		 c1--;
+	 }
+}
+}
